734173285256bfd4eddc7901442b432edf3192f7
Patricio Vargas <pevargas2730@gmail.com>
2019-09-30 03:24:51 -0400

    creating the interface

---
+++ libs/employee/README.md
@@ -0,0 +1,7 @@
+# employee
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `ng test employee` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/employee/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'employee',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/employee'
+};
