1dc016be3a30883988470a9a5172360072220c2e
Patricio Vargas <pevargas2730@gmail.com>
2019-09-30 04:28:59 -0400

    added the ui module to our app. module

--- apps/employees/src/app/app.module.ts
+++ apps/employees/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { UiModule } from '@thisdotx/ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    UiModule,
+    HttpClientModule,
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
