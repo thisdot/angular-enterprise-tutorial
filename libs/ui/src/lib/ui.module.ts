f61add57280933abd06405c2fa4613b2063bbf04
Patricio Vargas <pevargas2730@gmail.com>
2019-09-30 03:30:31 -0400

    created employee list component

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [EmployeeListComponent],
+  exports: [EmployeeListComponent]
 })
 export class UiModule {}
