e77e383fdd49332362813e88d3e7ef7d67fabb19
Patricio Vargas <pevargas2730@gmail.com>
2019-09-30 04:28:29 -0400

    added httpclien to our lib module

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   declarations: [EmployeeListComponent],
   exports: [EmployeeListComponent]
 })
